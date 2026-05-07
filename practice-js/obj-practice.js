fd070bebb9f1ce5859548102c8d6402a969a0114
Yumiko Matsubara <258401@st.takushoku-u.ac.jp>
2026-05-07 16:45:21 +0900

    class enshuu

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,13 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
+console.log(campus.address);
+for (let n of campus.buildingD){
+  console.log(n);
+}
+for ( let nn of gakka){
+  console.log(nn.name)
+}
 
+
+ 
