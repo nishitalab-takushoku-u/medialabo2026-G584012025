8f147e49408fc2067656c6da1db66939bc4b9358
Yumiko Matsubara <258401@st.takushoku-u.ac.jp>
2026-05-21 18:14:51 +0900

    up

---
+++ practice-dom2/dom2-intro.js
@@ -0,0 +1,36 @@
+function changeDom(){
+    let l = document.createElement('li');
+    l.textContent = 'ヨット';
+    let u = document.querySelector('ul#kazoeuta');
+    u.insertAdjacentElement('beforeend', l);
+    
+    let a = document.createElement('a');
+    a.textContent = '拓殖大学HP';
+    a.setAttribute('href', 'https//www.takushoku-u.ac.jp');
+    let p = document.querySelector('p#takudai');
+    p.insertAdjacentElement('afterend', a );
+
+    let i = document.querySelector('img#bluemoon');
+    i.setAttribute('src', 'bluemoon.jpg');
+
+    l = document.querySelector('li#mochi');
+    l.remove();
+    u = document.querySelector('ul#kassen');
+    u.remove();
+
+    u=document.createElement('ul') 
+    p=document.querySelector('p#primary');
+    p.insertAdjacentElement('afterend' , u);
+    l = document.createElement('li'); 
+    l.textContent = '赤';
+    u.insertAdjacentElement('beforeend', l);
+    l = document.createElement('li');
+    l.textContent = '緑'; 
+    u.insertAdjacentElement('beforeend', l); 
+    l = document.createElement('li');
+    l.textContent='青';
+    u.insertAdjacentElement('beforeend', l);
+}
+
+b = document.querySelector('button#henkou');
+b = addEventListener('click', changeDom);
