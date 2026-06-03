0150684b165fed781bed44101db2052c9a372ee8
Yumiko Matsubara <258401@st.takushoku-u.ac.jp>
2026-06-03 22:59:00 +0900

    please

--- weather.js
+++ weather.js
@@ -1,27 +1,58 @@
 
 // 課題3-2 のプログラムはこの関数の中に記述すること
 function print(data) {
-  console.log(data)
+  console.log(data['coord']['lon']);
+  console.log(data['coord']['lat']);
+  console.log(data['weather'][0]['description']);
+  console.log(data['main']['temp_min']);
+  console.log(data['main']['temp_max']);
+  console.log(data['wind']['speed']);;
 }
 
 // 課題5-1 の関数 printDom() はここに記述すること
-function printDom(data) {
+let MVP = document.body;
+MVP=addEventListener('click', printDom);
 
+function printDom(data) {
+  let one = document.createElement('div', 'result');
+  let two = document.querySelector('#henkouori');
+  two.insertAdjacentElement('afterend', one);
+  let three = document.createElement('li');
+  three.textContent=data.coord.lon;
+  
 }
+
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
-
+let m = document.querySelector('#henkou1');
+m.addEventListener('click', sendRequest);
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
-
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/2643743.json';
+  axios.get(url)
+        .then(showResult)   
+        .catch(showError)   
+        .then(finish); 
+/*function sendRequest()*/ 
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+let data = resp.data;
 
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    // data をコンソールに出力
+    console.log(data);
+
+    // data.x を出力
+    console.log(data.x);
 }
 
 // 課題6-1: 通信エラーが発生した時の処理
@@ -84,4 +115,7 @@ let data = {
   "name": "北京市",
   "cod": 200
 };
+
+
+
 
